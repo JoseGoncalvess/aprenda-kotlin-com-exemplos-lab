0b6056371679ba733e068e01f34478c71d05b913
José Gonçalves <105722921+JoseGoncalvess@users.noreply.github.com>
2023-08-23 20:17:35 -0300

    end of implemetation abistration

--- desafio.kt
+++ desafio.kt
@@ -1,19 +1,32 @@
+// Kotlin-PlayGound :(https://pl.kotl.in/2IJEbHV80)
+
 enum class Nivel { BASIC, INTERMEDIARY, ADVANCED }
 
 class Usuario(var name: String, var age: Int )
 
-data class ConteudoEducacional(var nomeConteudo: String, var Nivel: Nivel, val duration: Int = 30)
+data class ConteudoEducacional(var NomeConteudo: String, var Nivel: Nivel, val Duration: Int = 30)
 
 data class Formacao(val name: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        inscritos.add(usuario)
-        println("Parabéns ${usuario.name} por fazer parte do curso de $name")
-        println("Neste curso ${usuario.name},  Você vai aprender sobre $conteudos.!")
-
-        // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(user: Usuario?) {
+        when (user){
+            null -> println("For cursa $name, we need your information to register!")
+            user -> {
+        inscritos.add(user)
+        println("Hello ${user.name}, Welcome to Dio.me, Congratulations on choosing to be part of $name's course")
+        println("${user.name}, Check out the list of contents you will learn:")
+        for (c in conteudos){
+            println(c)
+        }
+                
+            } else ->{
+                println("Are you sure you don't want to take our training $name")
+            }
+        }
+        
+       
     }
 }
 
@@ -27,19 +40,12 @@ val user2: Usuario = Usuario(name= "Gui", age=18)
 //Lista de conteúdos
 var listConteudos = mutableListOf<ConteudoEducacional>(ConteudoEducacional("Logica de Programação", Nivel.BASIC, 30),ConteudoEducacional("Orientação a Objeto [POO]", Nivel.INTERMEDIARY, 30))
 
-var CursoFlutetr = Formacao("Desenvolvimento Flutter FullStack",listConteudos)
-var CursoJava = Formacao("Desenvolvimento Bakend Java",listConteudos)
+var CursoFlutetr = Formacao("Development Flutter FullStack",listConteudos)
+var CursoJava = Formacao("Development backend Java",listConteudos)
 
 
 println(CursoFlutetr.matricular(user1))
+println()
 println(CursoJava.matricular(user2))
 
-
-
-
-   
-   
-    
-    // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
